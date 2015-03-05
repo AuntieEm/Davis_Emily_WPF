d1c79ed2993eeebfdca406394bea89ccf2c56048
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-05 12:12:32 -0600

    Practice

---
+++ Arrays/js/script.js
@@ -0,0 +1,20 @@
+// Arrays
+
+var muppettNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo", 38]; //o,1,2,3,4
+
+console.log(muppettNames[0]);
+
+console.log(muppettNames[1]);
+
+console.log(muppettNames[2]);
+
+console.log(muppettNames[3]);
+
+console.log(muppettNames[4]);
+
+muppetNames[1] = "Rizzo"; //change index to Rizzo
+console.log(muppettNames[1]); //lets me se whats in the index #1
+
+muppettnames[2] = "Fozzy";
+
+var num = 2;

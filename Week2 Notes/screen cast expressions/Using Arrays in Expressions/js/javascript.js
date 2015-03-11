96baa159dfa67f21b052b33c58f20367b9a86ce2
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-10 23:10:01 -0500

    more screen casts

---
+++ Week2 Notes/screen cast expressions/Using Arrays in Expressions/js/javascript.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Alys1 on 3/10/15.
+ */
+//Using arrays in Expressions
+// working with lists
+
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+
+console.log(total);

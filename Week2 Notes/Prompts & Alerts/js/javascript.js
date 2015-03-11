aae933571b5966323a51718f2309afd7ac7476ce
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-10 23:23:12 -0500

    Prompts & Alerts
    Learned how to use prompts and alerts together.

---
+++ Week2 Notes/Prompts & Alerts/js/javascript.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Alys1 on 3/10/15.
+ */
+//Prompts & Alerts
+//Getting info in and out
+
+/*
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
+*/
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width."); // "\n = line break
+var height = prompt("Please enter the height.");
+var area = width * height; //calculates with that info
+//console.log(area); //prints it out to the console.
+alert("The area of your rectangle is " + area);
+
+/*
+var result = "The area of your rectangle is " + area;
+alert(result);
+ */

f3e1085aa04c70eac9f1d34a548386a67cf80b36
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-17 23:43:27 -0500

    Screencast:  Conditionals Example
    basic conditional statement
    decision making for the machine
    conditional logic

---
+++ Week 3/Screencasts/java script/script.js
@@ -0,0 +1,18 @@
+/*Screencast:  Conditionals Example
+Basic Conditional Statement
+    Decision making for the machine
+    Conditional Logic
+    */
+
+//var oldEnough = true;
+var oldEnough = false;
+//if the child is old enough, print to the console "you can ride!"
+
+if (oldEnough){
+    //code performed in condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("What comes after?");
+
+
+

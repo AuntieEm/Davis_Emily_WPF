b2df6f50d410b7e72b4daf67737dadbe0b744949
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-18 01:00:26 -0500

    Screencast:  Ternary Operators
    decision making on one line

--- Week 3/Screencasts/java script/script.js
+++ Week 3/Screencasts/java script/script.js
@@ -245,3 +245,48 @@ if(iPhonePrice2 < budget2 || wonLottery === true){
 }else{
     console.log("No lottery. No phone!");
 }
+
+
+/*
+Screencast:  Ternary Operators
+decision making on one line
+ */
+
+/*if(condition){
+    do if true;
+}else{
+    do if false;
+}
+
+(condition) ? do if true : do if false;
+*/
+
+
+var gpa = 48;
+//if the gpa is over the min 2.0 score the student can graduate
+/*
+if(gpa > 2.0){
+    console.log("You can graduate");
+}else{
+    console.log("GPA is too low");
+}
+*/
+
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
+
+
+var age = 11;
+var book;
+
+//if the child is under 10 they get green eggs and ham, otherwise they get the time machine
+
+if(age < 10){
+    book =  "Green Eggs and Ham";
+}else{
+    book = "The Time Machine";
+}
+console.log(book);
+
+
+book = (age > 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);

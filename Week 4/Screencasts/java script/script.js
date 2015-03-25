4c66be96e52d42f51aa227eee894772d9256118c
AuntieEm <Emily.davis.5828@gmail.com>
2015-03-24 22:08:27 -0500

    Screencast 1
    Basic Function Structure

---
+++ Week 4/Screencasts/java script/script.js
@@ -0,0 +1,17 @@
+//Screencast:  Basic Function Structure
+
+function outptMsg() {
+    console.log("Hello World.");
+}
+
+function calcArea() {
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+
+
+
+//Screencast:  Function Invocation - Telling the function to run
